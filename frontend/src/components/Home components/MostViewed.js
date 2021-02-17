977c4c4e9eb27f07e92fe49b393b981056271c47
nkangi jafari <nkangijafari@gmail.com>
2021-02-17 23:46:30 +0400

    Added the mostViewed state to change images on click events

--- frontend/src/components/Home components/MostViewed.js
+++ frontend/src/components/Home components/MostViewed.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SectionHeading from './SectionHeading';
 
 const MostViewed = () => {
+	const [pictureSrc, setPicture] = useState('images/watch1.png');
 	return (
 		<section className='feature' id='featured'>
 			<SectionHeading desc='Most viewed product' />
@@ -9,39 +10,72 @@ const MostViewed = () => {
 			<div className='row'>
 				<div className='image-container'>
 					<div className='big-image'>
-						<img src='/images/watch1.png' alt='' />
+						<img src={pictureSrc} alt='' />
 					</div>
 					<div className='small-image'>
-						<img className='image-active' src='images/watch1.png' alt='' />
-						<img src='/images/watch2.png' alt='mostViewed' />
-						<img src='/images/watch3.png' alt='mostViewed' />
-						<img src='/images/watch4.png' alt='mostViewed' />
+						<img
+							className='image-active'
+							src='images/watch1.png'
+							alt='mostViewed'
+							onClick={() => {
+								setPicture('/images/watch1.png');
+							}}
+						/>
+						<img
+							src='/images/watch2.png'
+							alt='mostViewed'
+							onClick={() => {
+								setPicture('/images/watch2.png');
+							}}
+						/>
+						<img
+							src='/images/watch3.png'
+							alt='mostViewed'
+							onClick={() => {
+								setPicture('/images/watch3.png');
+							}}
+						/>
+						<img
+							src='/images/watch4.png'
+							alt='mostViewed'
+							onClick={() => {
+								setPicture('/images/watch4.png');
+							}}
+						/>
 					</div>
 				</div>
-				<div className='content'>
-					<h3>smart watches</h3>
-					<div className='stars'>
-						<i className='fas fa-star' />
-						<i className='fas fa-star' />
-						<i className='fas fa-star' />
-						<i className='fas fa-star' />
-						<i className='fas fa-star' />
-						<span>(500+) reviews</span>
-					</div>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-						at!
-					</p>
-					<strong className='price'>
-						₹1000 <span>₹1500</span>{' '}
-					</strong>
-					<a href='#'>
-						<button className='btn'>buy now</button>
-					</a>
-				</div>
+
+				<ProductDetails />
 			</div>
 		</section>
 	);
 };
 
+const ProductDetails = () => {
+	return (
+		<>
+			<div className='content'>
+				<h3>smart watches</h3>
+				<div className='stars'>
+					<i className='fas fa-star' />
+					<i className='fas fa-star' />
+					<i className='fas fa-star' />
+					<i className='fas fa-star' />
+					<i className='fas fa-star' />
+					<span>(500+) reviews</span>
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+					at!
+				</p>
+				<strong className='price'>
+					₹1000 <span>₹1500</span>{' '}
+				</strong>
+				<a href='#'>
+					<button className='btn'>buy now</button>
+				</a>
+			</div>
+		</>
+	);
+};
 export default MostViewed;
