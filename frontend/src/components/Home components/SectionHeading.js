6b39a7788db8c4cebb740abb4541c6ee3764da83
nkangi jafari <nkangijafari@gmail.com>
2021-02-26 12:33:34 +0400

    Changed alot in the cart page, structure, styling and functioning

--- frontend/src/components/Home components/SectionHeading.js
+++ frontend/src/components/Home components/SectionHeading.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const SectionHeading = ({ desc }) => {
+const SectionHeading = ({ desc, marginB }) => {
 	return (
-		<div className='sectionHeading'>
+		<div className='sectionHeading' style={{ marginBottom: marginB }}>
 			<div className='sectionHeading__line'></div>
 			<h2 className='sectionHeading__h2'>{desc}</h2>
 			<div className='sectionHeading__line'></div>
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -14,6 +14,7 @@ import {
 import { addToCart, removeFromCart } from '../actions/cartActions';
 
 import EmptyDisplay from '../components/EmptyDisplay';
+import SectionHeading from '../components/Home components/SectionHeading';
 
 const CartScreen = (props) => {
 	const { match, location, history } = props;
@@ -45,88 +46,112 @@ const CartScreen = (props) => {
 	};
 
 	return (
-		<Row style={{ padding: '2rem 2rem' }}>
-			<Col md={8}>
-				<h1>Shopping Cart</h1>
-				{cartItems.length === 0 ? (
-					<EmptyDisplay />
-				) : (
-					<ListGroup variant='flush'>
-						{cartItems.map((item) => (
-							//product is the id
-							<ListGroup.Item key={item.product}>
-								<Row>
-									<Col md={1}>
-										<Image src={item.image} alt={item.name} fluid rounded />
-									</Col>
+		<>
+			<SectionHeading desc='Shopping Cart' marginB='1.5rem' />
 
-									<Col md={5}>
-										<Link to={`/products/${item.product}`}>{item.name}</Link>
-									</Col>
-									<Col md={2}>${item.price}</Col>
-									<Col md={2}>
-										<Form.Control
-											as='select'
-											variant='primary'
-											value={Number(item.quantity)}
-											onChange={(e) =>
-												dispatch(
-													addToCart(item.product, Number(e.target.value))
-												)
-											}>
-											{
-												//Get the number of pro
-												[...Array(item.countInStock).keys()].map((x) => (
-													<option key={x + 1} value={x + 1}>
-														{' '}
-														{x + 1}{' '}
-													</option>
-												))
-											}
-										</Form.Control>
-									</Col>
-									<Col md={2}>
-										<button
-											className='btn btn--cartProductDelete'
-											type='button'
-											variant='light'
-											onClick={() => removeFromCartHandler(item.product)}>
-											<i className='fas fa-trash'></i>
-										</button>
-									</Col>
-								</Row>
+			<Row style={{ padding: '2rem 2rem' }} className='cart'>
+				<Col md={9} sm={12} className='cart__left'>
+					{cartItems.length === 0 ? (
+						<EmptyDisplay />
+					) : (
+						<ListGroup variant='flush'>
+							{cartItems.map((item) => (
+								//product is the id
+								<ListGroup.Item key={item.product}>
+									<Row className='cart__items'>
+										<Col md={1} sm={2} className='cart__items--imgWrapper'>
+											<Image
+												src={item.image}
+												alt={item.name}
+												fluid
+												className='cart__items--imgWrapper-img'
+											/>
+										</Col>
+										<Col md={11} sm={10} className='cart__items--details'>
+											<div className='cart__items--details-name'>
+												<Link to={`/products/${item.product}`}>
+													<strong>{item.name}</strong>
+												</Link>
+												<div className='cart__items--details-info'>
+													Size: 3L
+												</div>
+												<div className='cart__items--details-info'>
+													Color: Black
+												</div>
+											</div>
+											<div className='cart__items--details-price'>
+												${item.price}
+											</div>
+											<Form className='cart__items--details-form'>
+												<Form.Control
+													as='select'
+													variant='primary'
+													value={Number(item.quantity)}
+													onChange={(e) =>
+														dispatch(
+															addToCart(item.product, Number(e.target.value))
+														)
+													}>
+													{
+														//Get the number of pro
+														[...Array(item.countInStock).keys()].map((x) => (
+															<option key={x + 1} value={x + 1}>
+																{' '}
+																{x + 1}{' '}
+															</option>
+														))
+													}
+												</Form.Control>
+											</Form>
+											<div className='cart__items--details-price'>
+												<strong>
+													${(item.quantity * item.price).toFixed(2)}
+												</strong>
+											</div>
+											<div>
+												<button
+													className='btn btn--cartProductDelete'
+													type='button'
+													variant='light'
+													onClick={() => removeFromCartHandler(item.product)}>
+													<i className='fas fa-trash'></i>
+												</button>
+											</div>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+							))}
+						</ListGroup>
+					)}
+				</Col>
+				<Col md={3} sm={8} className='cart__right'>
+					<Card className='cart__right--card'>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<h3>
+									subtotal (
+									{cartItems.reduce((acc, item) => acc + item.quantity, 0)})
+									Items
+								</h3>
+								$
+								{cartItems
+									.reduce((acc, item) => acc + item.quantity * item.price, 0)
+									.toFixed(2)}
 							</ListGroup.Item>
-						))}
-					</ListGroup>
-				)}
-			</Col>
-			<Col md={4}>
-				<Card>
-					<ListGroup variant='flush'>
-						<ListGroup.Item>
-							<h3>
-								{' '}
-								subtotal (
-								{cartItems.reduce((acc, item) => acc + item.quantity, 0)}) Items
-							</h3>
-							$
-							{cartItems
-								.reduce((acc, item) => acc + item.quantity * item.price, 0)
-								.toFixed(2)}
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Button
-								type='button'
-								className='btn-block'
-								disabled={cartItems.length === 0}
-								onClick={checkOutHandler}>
-								Proceed To CheckOut
-							</Button>
-						</ListGroup.Item>
-					</ListGroup>
-				</Card>
-			</Col>
-		</Row>
+							<ListGroup.Item>
+								<Button
+									type='button'
+									className='btn-block'
+									disabled={cartItems.length === 0}
+									onClick={checkOutHandler}>
+									Proceed To CheckOut
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
 	);
 };
 
