5f298df1eb75cb6065ac21d5500a9327429d00b1
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-05-04 09:34:41 +0400

    Order screens combined in styling

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Button, Row, Col, Table } from 'react-bootstrap';
+import { Button, Row, Col, Table } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
@@ -68,53 +68,56 @@ const ProfileScreen = ({ history }) => {
 	};
 	return (
 		//Form container is a component, and inside details are the children
-		<Row>
-			<Col>
+		<Row className='profile'>
+			<Col md={3}>
 				<h1>User Profile</h1>
 				{message && <Message variant='danger'>{message}</Message>}
 				{error && <Message variant='danger'>{error}</Message>}
 				{success && <Message variant='success'>{'Profile Updated'}</Message>}
 				{loading && <Loader />}
-				<Form onSubmit={submitHandler}>
-					<Form.Group controlId='name'>
-						<Form.Label>Enter Name</Form.Label>
-						<Form.Control
+
+				<form onSubmit={submitHandler}>
+					<div>
+						<label htmlFor='name'>Enter Name</label>
+						<input
+							id='name'
 							type='text'
 							placeholder='Enter Your Name'
 							value={name}
-							onChange={(e) => setName(e.target.value)}></Form.Control>
-					</Form.Group>
-					<Form.Group controlId='email'>
-						<Form.Label>Email Address</Form.Label>
-						<Form.Control
+							onChange={(e) => setName(e.target.value)}></input>
+					</div>
+					<div>
+						<label htmlFor='email'>Email Address</label>
+						<input
+							id='email'
 							type='email'
 							placeholder='Enter Your Email'
 							value={email}
-							onChange={(e) => setEmail(e.target.value)}></Form.Control>
-					</Form.Group>
-					<Form.Group controlId='password'>
-						<Form.Label>Password</Form.Label>
-						<Form.Control
+							onChange={(e) => setEmail(e.target.value)}></input>
+					</div>
+					<div>
+						<label htmlFor='password'>Password</label>
+						<input
+							id='password'
 							type='password'
 							placeholder='Enter Your Password'
 							value={password}
-							onChange={(e) => setPassword(e.target.value)}></Form.Control>
-					</Form.Group>
-					<Form.Group controlId='confirmPassword'>
-						<Form.Label>Confirm Password</Form.Label>
-						<Form.Control
+							onChange={(e) => setPassword(e.target.value)}></input>
+					</div>
+					<div>
+						<label htmlFor='confirmPassword'>Confirm Password</label>
+						<input
+							id='confirmPassword'
 							type='password'
 							placeholder='Confirm Your Password'
 							value={confirmPassword}
-							onChange={(e) =>
-								setConfirmPassword(e.target.value)
-							}></Form.Control>
-					</Form.Group>
+							onChange={(e) => setConfirmPassword(e.target.value)}></input>
+					</div>
 
 					<Button type='submit' variant='primary'>
 						Update
 					</Button>
-				</Form>
+				</form>
 			</Col>
 			<Col md={9}>
 				<h3>My Orders</h3>
