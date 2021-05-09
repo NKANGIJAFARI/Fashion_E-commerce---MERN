cc8159dab1604f538c104909150f5056d39ad657
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-05-09 11:27:41 +0400

    4th deploy attempt

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	//Below we check any route that is not an api, and will be captured to index.html
 	//which is in the build folder
-	app.use('*', (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
 } else {
