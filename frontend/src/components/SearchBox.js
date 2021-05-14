5cdb8085b32387bbed2abd52a64b76f00c81c022
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-05-14 08:57:11 +0400

    Adedd a function to fire while input gets or loose focus

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ history }) => {
 	const [keyword, setKeyword] = useState();
 	const [showSearch, setShowSearch] = useState(false);
 
+	//Whenever the search bar is submitted with a keyword, this func will run
 	const submitHandler = (e) => {
 		e.preventDefault();
 		if (keyword && keyword.length !== 0) {
@@ -21,12 +22,24 @@ const SearchBox = ({ history }) => {
 		/*Whenever the search input is shown,  it will cover some elements on the page,
 		so if the user doesnt start typing in anything to the input, we shall close the 
 		search input and only display the search icon*/
-		if (!keyword && showSearch) {
+
+		if (focus) {
 			setTimeout(() => {
 				setShowSearch(false);
 			}, 6000);
 		}
+
+		console.log('Show Search', showSearch);
 	}, [keyword, showSearch]);
+
+	const manageFocus = (e) => {
+		console.log(e, e.type);
+		if (e.type === 'focus') {
+			setFocus(true);
+		} else if (e.type === 'blur') {
+			setFocus(false);
+		}
+	};
 
 	return (
 		<div className={`searchForm ${showSearch ? 'inputShown' : ''}`}>
@@ -36,12 +49,17 @@ const SearchBox = ({ history }) => {
 				<form onSubmit={submitHandler}>
 					<div className='searchForm__inputGroup'>
 						<input
-							autocomplete='off'
+							autoComplete='off'
 							type='text'
-							onChange={(e) => setKeyword(e.target.value.trim())}
+							onFocus={(e) => {
+								manageFocus(e);
+							}}
+							onBlur={(e) => {
+								manageFocus(e);
+							}}
+							//onChange={(e) => setKeyword(e.target.value.trim())}
 							className='mr-sm-2 ml-sm-5 searchForm__input'
 							placeholder='search Products ...'></input>
-
 						<button type='submit' className='p-2 searchForm__button'>
 							<i className='fas fa-search'></i>
 						</button>
